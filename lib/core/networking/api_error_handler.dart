9a3840c8b46c260dc55802872477d387764d1c25
Maha Farghaly <mahafarghalymo@gmail.com>
2025-07-15 18:11:54 +0300

    Remove unused code

--- lib/core/networking/api_error_handler.dart
+++ lib/core/networking/api_error_handler.dart
@@ -1,45 +1,14 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
-
 import 'api_error_model.dart';
-// ApiErrorModel parseApiError(dynamic error) {
-//   if (error is DioException) {
-//     final data = error.response?.data;
-//     if (data is Map<String, dynamic>) {
-//       return ApiErrorModel.fromJson(data);
-//     }
-//     //"Invalid server error format"
-//     print("%%% dio");
-//     return  ApiErrorModel(errors: data.toString());
-//   }
-// print("%%% not dio");
-//   return ApiErrorModel(errors: error is String ? error : jsonEncode(error));
-//   //return ApiErrorModel(errors: error.toString());
-// }
+
 ApiErrorModel parseApiError(dynamic error) {
   try {
     if (error is DioException) {
       final data = error.response?.data;
-
-      if (data is Map<String, dynamic>) {
-        return ApiErrorModel.fromJson(data);
-      }
-
-      return ApiErrorModel(errors: data.toString());
+      return ApiErrorModel.fromJson(data);
     }
-
-    // Handle StateError or anything else that isn't encodable
-    if (error is StateError) {
-      return ApiErrorModel(errors: error.message); // ✅ shows actual message
-    }
-
-    if (error is Map<String, dynamic>) {
-      return ApiErrorModel(errors: jsonEncode(error));
-    }
-
-    return ApiErrorModel(errors: error.toString()); // fallback to string
+    return const ApiErrorModel(errors: "Server Exception");
   } catch (e) {
-    return ApiErrorModel(errors: 'Unexpected error occurred');
+    return const ApiErrorModel(errors: 'Unexpected error occurred');
   }
 }
